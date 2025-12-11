d017052605115800c80aa76dc92e0d820abb4f57
Bilel Jellouli <bilel.jellouli@gmail.com>
2025-12-11 22:55:07 +0100

    Initialize code sniffers and static analytics

---
+++ rector.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        instanceOf: true,
+        earlyReturn: true,
+    )
+    ->withAttributesSets(
+        phpunit: true
+    )
+    ->withPhpSets()
+    ->withRules([
+        DeclareStrictTypesRector::class,
+    ])
+    ->withCache(__DIR__.'/storage/framework/cache/rector');
