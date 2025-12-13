f2f4c2c4ffbd4504e47ae1f8c18406611d39aa39
Bilel Jellouli <bilel.jellouli@gmail.com>
2025-12-13 21:29:05 +0100

    Orderbook controller

---
+++ app/Http/Controllers/OrderBooksController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\AssetSymbols;
+use App\Enums\OrderSide;
+use App\Enums\OrderStatus;
+use App\Http\Resources\OrderResource;
+use App\Models\Order;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Enum;
+
+class OrderBooksController extends Controller
+{
+    public function __invoke(
+        Request $request
+    ): JsonResponse {
+        $request->validate([
+            'symbol' => ['required', new Enum(AssetSymbols::class)],
+        ]);
+
+        $symbol = $request->symbol;
+
+        $buyOrders = Order::query()
+            ->where('symbol', $symbol)
+            ->where('side', OrderSide::BUY)
+            ->where('status', OrderStatus::OPEN)
+            ->orderBy('price', 'desc')
+            ->get();
+
+        $sellOrders = Order::query()
+            ->where('symbol', $symbol)
+            ->where('side', OrderSide::SELL)
+            ->where('status', OrderStatus::OPEN)
+            ->orderBy('price', 'asc')
+            ->get();
+
+        return response()->json([
+            'symbol' => $symbol,
+            'buy' => OrderResource::collection($buyOrders),
+            'sell' => OrderResource::collection($sellOrders),
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\CancelOrdersController;
+use App\Http\Controllers\OrderBooksController;
 use App\Http\Controllers\StoreOrdersController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:api')->get('/user', function () {
+    Route::get('orders', OrderBooksController::class)->name('api.orders.books');
     Route::post('orders', StoreOrdersController::class)->name('api.orders.store');
     Route::post('orders/{order}/cancel', CancelOrdersController::class)->name('api.orders.cancel');
 });
