235a23b6f3e4158bb13a852653f325fa017f3dfc
Bilel Jellouli <bilel.jellouli@gmail.com>
2025-12-13 19:57:17 +0100

    Add creating buying/selling order logic

---
+++ app/Actions/CreateBuyingOrder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Actions;
+
+use App\Enums\AssetSymbols;
+use App\Enums\OrderSide;
+use App\Enums\OrderStatus;
+use App\Events\BuyingOrderCreated;
+use App\Exceptions\InsufficientBalanceException;
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class CreateBuyingOrder
+{
+    public function execute(
+        User $user,
+        AssetSymbols $assetSymbols,
+        float $price,
+        float $quantity,
+    ): Order {
+        return DB::transaction(function () use ($user, $assetSymbols, $price, $quantity) {
+            $total = $price * $quantity;
+
+            if ($user->balance < $total) {
+                throw new InsufficientBalanceException();
+            }
+
+            $user->lockForUpdate()->update([
+                'balance' => $user->balance - $price * $quantity,
+            ]);
+
+            $order = $user->orders()->create([
+                'side' => OrderSide::BUY,
+                'symbol' => $assetSymbols,
+                'quantity' => $quantity,
+                'price' => $price,
+                'status' => OrderStatus::OPEN,
+            ]);
+
+            BuyingOrderCreated::dispatch($order);
+
+            return $order;
+        });
+    }
+}
---
+++ app/Actions/CreateSellingOrder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Actions;
+
+use App\Enums\OrderSide;
+use App\Enums\OrderStatus;
+use App\Events\SellingOrderCreated;
+use App\Models\Asset;
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class CreateSellingOrder
+{
+    public function execute(
+        User $user,
+        Asset $asset,
+        float $quantity,
+        float $price,
+    ): Order {
+        return DB::transaction(function () use ($user, $asset, $quantity, $price) {
+            $asset->lockForUpdate()->update([
+                'amount' => $asset->amount - $quantity,
+                'locked_amount' => $asset->locked_amount + $quantity,
+            ]);
+
+            $order = $user->orders()->create([
+                'side' => OrderSide::SELL,
+                'symbol' => $asset->symbol,
+                'quantity' => $quantity,
+                'price' => $price,
+                'status' => OrderStatus::OPEN,
+            ]);
+
+            SellingOrderCreated::dispatch($order);
+
+            return $order;
+        });
+    }
+}
